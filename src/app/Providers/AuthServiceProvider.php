0f568215e2e1d86437af8ccb91d35685ca1010ba
kasahara <kasahara.maki.info@gmail.com>
2025-09-14 12:03:39 +0900

    メール日本語化

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -30,9 +30,11 @@ class AuthServiceProvider extends ServiceProvider
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             return (new MailMessage)
                 ->subject('coachtechフリマ仮登録完了メール')
-                ->line('クリックで本登録を完了してください。')
+                ->greeting('こんにちは')
+                ->line('本登録ボタンをクリックして本登録を完了してください。')
                 ->action('本登録', $url)
-                ->line('このメールに心当たりのない場合は、このメールを破棄してください。');
+                ->line('このメールに心当たりのない場合は、このメールを破棄してください。')
+                ->salutation('coachtechフリマ');
         });
     }
 }
