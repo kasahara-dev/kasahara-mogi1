9952248c5ecd090a7bc219bd0dc0456787bd828b
kasahara <kasahara.maki.info@gmail.com>
2025-10-06 10:53:17 +0900

    マイグレーション修正

--- src/database/migrations/2025_07_30_212741_create_items_table.php
+++ src/database/migrations/2025_07_30_212741_create_items_table.php
@@ -16,7 +16,7 @@ class CreateItemsTable extends Migration
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->string('img_path', 255)->nullable();
+            $table->string('img_path', 255);
             $table->tinyInteger('condition');
             $table->string('name');
             $table->string('brand')->nullable();
