af876262f98b32b26d23b660eedf82dd01bfa2ff
kasahara <kasahara.maki.info@gmail.com>
2026-03-06 13:58:18 +0900

    テスト

---
+++ src/tests/Feature/CaseProTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+class CaseProTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_取引チャット()
+    {
+        // 
+    }
+    public function test_評価確認(){
+        // 
+    }
+    public function test_チャット投稿(){
+        // 
+    }
+    public function test_チャット編集削除(){
+        // 
+    }
+    public function test_評価(){
+        // 
+    }
+    public function test_メール通知(){
+        // 
+    }
+}
